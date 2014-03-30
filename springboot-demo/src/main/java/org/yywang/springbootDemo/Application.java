ea4b4c0d635d30fb42be5fec6481f5bca706661a
wangyan <wangyan9110@gmail.com>
2014-03-30 17:31:22 +0800

    修改包名

--- springboot-demo/src/main/java/org/yywang/springbootDemo/Application.java
+++ springboot-demo/src/main/java/org/yywang/springbootDemo/Application.java
@@ -1,4 +1,4 @@
-package com.iflytek.springbootDemo;
+package org.yywang.springbootDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
