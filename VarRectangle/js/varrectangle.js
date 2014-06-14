150f1b7516f75214ac9f026bd76e27aaa891097e
wangyan <wangyan9110@gmail.com>
2014-06-14 14:47:35 +0800

    add VarRectangle

---
+++ VarRectangle/js/varrectangle.js
@@ -0,0 +1,13 @@
+/**
+ * @author yywang5
+ */
+window.varRect=window.varRect||{};
+
+(function(varRect){
+
+    function init(canvas){
+        canvas.moveTo(10,100);
+    }
+
+    varRect.init=init;
+})(window.varRect);
